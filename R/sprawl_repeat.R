263b38b0af7de596322f08d0d094575a43ca4321
Lou96cpu <liuxiaowei1996@hotmail.com>
2026-09-07 13:13:04 +0200

    Refactor sprawl_ex function with documentation and params
    Updated the sprawl_ex function to include detailed documentation and changed parameter names for clarity.

--- R/sprawl_repeat.R
+++ R/sprawl_repeat.R
@@ -1,13 +1,28 @@
-sprawl_ex <- function(matrix, urban_values, p) {
+#' Repeat urban sprawl
+#'
+#' @param x A matrix containing land-use classes.
+#' @param urban_values A numeric vector of values representing urban land use.
+#' @param expansion Proportional increase in urban cells.
+#' @return A list containing the updated matrix and expansion ratio.
+
+sprawl_ex <- function(x, urban_values, expansion) {
 
   total_conversions = 0
-  updated_matrix = matrix
+  updated_matrix = x
+
+  original_urban_count <- sum(
+    x %in% urban_values,
+    na.rm = TRUE
+  )
+     
+  target_conversions <- ceiling(
+    original_urban_count * expansion)
 
   repeat {
 
-    result <- urban_sprawl(updated_matrix, urban_values, (p - total_conversions))
+    result <- urban_sprawl(updated_matrix, urban_values, (target_conversions - total_conversions))
     updated_matrix <- result$matrix
-    total_conversions <- total_conversions + result$current_ratio
+    total_conversions <- total_conversions + result$expansion
     if (total_conversions >= p) break
 
   }
